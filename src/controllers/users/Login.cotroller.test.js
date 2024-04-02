044f796934346a29bd341c146ac777c077769447
Alex Santos <alexsantos.web.developer@gmail.com>
2024-04-02 18:01:08 -0300

    Added: Login controller tests

--- src/controllers/users/Login.cotroller.test.js
+++ src/controllers/users/Login.cotroller.test.js
@@ -2,6 +2,7 @@ import request from 'supertest'
 import app from '../../../index.js'
 
 describe('Users authentication login /usr/login', () => {
+
     const user = {
         reg: 'adm-085311-24-DA',
         password: '7803-Aob'
@@ -10,5 +11,28 @@ describe('Users authentication login /usr/login', () => {
     it('Should be return a token', async () => {
         const res = await request(app).post('/usr/login').send(user)
         expect(res.status).toBe(200)
+        expect(res.body.msg).toBe('user logged successfully! adm')
+    })
+
+    const nullUser = {
+        reg: '',
+        password: ''
+    }
+
+    it('Shold be return a not found error', async () => {
+        const res = await request(app).post('/usr/login').send(nullUser)
+        expect(res.status).toBe(404)
+        expect(res.body.error).toBe('User not founded!')
+    })
+
+    const userWrongPassword = {
+        reg: 'adm-085311-24-DA',
+        password: '8732yg2ry'
+    }
+
+    it('Shold be return a wrong password error', async () => {
+        const res = await request(app).post('/usr/login').send(userWrongPassword)
+        expect(res.status).toBe(409)
+        expect(res.body.error).toBe('Wrong password!')
     })
 })
