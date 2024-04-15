e1cebc5e38e32a8e3869c975c7c2541e0ff12cb2
Alex Santos <alexsantos.web.developer@gmail.com>
2024-04-15 20:01:16 -0300

    Update: Documantation and StudentsGradesServices

--- src/controllers/students/Students.grades.controllers.js
+++ src/controllers/students/Students.grades.controllers.js
@@ -5,7 +5,7 @@ async function findGradesByStudentId(req, res) {
         const { id } = req.params
         const result = await StudentsGradesServices.findGradesByStudentId(id)
         if (Object.keys(result).length > 0) {
-            const groupedGrades = await StudentsGradesServices.calculateAverageGrades(result)
+            const groupedGrades = StudentsGradesServices.calculateAverageGrades(result)
             res.status(200).json({ Grades: result, Averages: groupedGrades })
         } else {
             res.status(404).json({ err: 'No grades found!' })
